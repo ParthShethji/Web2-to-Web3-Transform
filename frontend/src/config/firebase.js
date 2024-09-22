7cad28663e4add7fc96542cbaf172545a5198f60
Parth Shethji <parthshethji2693@gmail.com>
2024-09-22 15:07:09 +0530

    Update firebase.js

--- frontend/src/config/firebase.js
+++ frontend/src/config/firebase.js
@@ -5,13 +5,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAvO8NPH9TL8WXVWvRadN-LT0YGfHE_80U',
-  authDomain: 'ethindia2023.firebaseapp.com',
-  projectId: 'ethindia2023',
-  storageBucket: 'ethindia2023.appspot.com',
-  messagingSenderId: '1002231812923',
-  appId: '1:1002231812923:web:af36dbe1be42914b421d1e',
-  measurementId: 'G-HLGK2RB2NE',
+  apiKey: ''
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
+  measurementId: '',
 };
 
 // Initialize Firebase
